e42ef7b3e8c580f0458570673d1245a43719070c
Francisca <fraan928@gmail.com>
2021-11-18 04:32:56 -0300

    Cleaning code

--- src/services/gratibox.js
+++ src/services/gratibox.js
@@ -2,6 +2,7 @@ import axios from "axios"
 import { api } from "./apiUrl"
 
 function signUp(body) {
+    console.log(process.env.DATABASE_URL)
     const promise = axios.post(`${api}/sign-up`, body);
     return promise;
 }
